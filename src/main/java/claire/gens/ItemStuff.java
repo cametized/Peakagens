6b1b3322b19a2db37f4e70f48c3df965a438470d
Cametized <cameron.reese.f@gmail.com>
2026-05-09 13:33:04 -0400

    help these are all placeholders

--- src/main/java/claire/gens/ItemStuff.java
+++ src/main/java/claire/gens/ItemStuff.java
@@ -1,13 +1,17 @@
 package claire.gens;
 
+import net.fabricmc.fabric.api.creativetab.v1.FabricCreativeModeTab;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Function;
@@ -20,13 +24,30 @@ public class ItemStuff {
         Registry.register(BuiltInRegistries.ITEM, itemKey, item);
         return item;
     }
-    public static final Item RadicalRadio = register("radical", Item::new, new Item.Properties());
+    public static final Item RadicalRadio = register(
+            "radical",
+            properties -> new FragmentItem(properties,new MobEffectInstance(MobEffects.STRENGTH,60*20,5),true),
+            new Item.Properties()
+    );
 
     public static final Item strength = register(
             "strengthfrag",
             properties -> new FragmentItem(properties,new MobEffectInstance(MobEffects.STRENGTH,60*20,1),true),
             new Item.Properties()
     );
+
+    public static final Item strengthmin = register(
+            "strengthmin",
+            properties -> new FragmentItem(properties,new MobEffectInstance(MobEffects.STRENGTH,60*20,1),true),
+            new Item.Properties()
+    );
+
+    public static final Item strengthmax = register(
+            "strengthmax",
+            properties -> new FragmentItem(properties,new MobEffectInstance(MobEffects.STRENGTH,60*20,1),true),
+            new Item.Properties()
+    );
+
     public static final Item swiftness = register(
             "swiftnessfrag",
             properties -> new FragmentItem(properties,new MobEffectInstance(MobEffects.SPEED,60*20,1),true),
@@ -78,6 +99,33 @@ public class ItemStuff {
             new Item.Properties()
     );
 
+    public static final ResourceKey<@NotNull CreativeModeTab> CUSTOM_CREATIVE_TAB_KEY = ResourceKey.create(
+            BuiltInRegistries.CREATIVE_MODE_TAB.key(), Identifier.fromNamespaceAndPath(Peakagens.MOD_ID, "creative_tab")
+    );
+    public static final CreativeModeTab CUSTOM_CREATIVE_TAB = FabricCreativeModeTab.builder()
+            .icon(() -> new ItemStack(ItemStuff.RadicalRadio))
+            .title(Component.translatable("creativeTab.peakagens"))
+            .displayItems((params, output) -> {
+                output.accept(ItemStuff.RadicalRadio);
+
+                output.accept(ItemStuff.strength);
+                output.accept(ItemStuff.strengthmin);
+                output.accept(ItemStuff.strengthmax);
+
+                output.accept(ItemStuff.haste);
+                output.accept(ItemStuff.swiftness);
+                output.accept(ItemStuff.jumpboos);
+                output.accept(ItemStuff.fireres);
+                output.accept(ItemStuff.resist);
+                output.accept(ItemStuff.absorption);
+                output.accept(ItemStuff.nightvision);
+                output.accept(ItemStuff.waterbreath);
+                output.accept(ItemStuff.invis);
+                output.accept(ItemStuff.lifesteal);
+            })
+            .build();
+
     public static void initialize() {}
 }
+
 
--- src/main/java/claire/gens/Peakagens.java
+++ src/main/java/claire/gens/Peakagens.java
@@ -2,8 +2,13 @@ package claire.gens;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static claire.gens.ItemStuff.CUSTOM_CREATIVE_TAB;
+import static claire.gens.ItemStuff.CUSTOM_CREATIVE_TAB_KEY;
 
 public class Peakagens implements ModInitializer {
 	public static final String MOD_ID = "peakagens";
@@ -14,5 +19,6 @@ public class Peakagens implements ModInitializer {
 		LOGGER.info("Hello Peak inc.. -Cam");
 
         ItemStuff.initialize();
+        Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, CUSTOM_CREATIVE_TAB_KEY, CUSTOM_CREATIVE_TAB);
 	}
 }
