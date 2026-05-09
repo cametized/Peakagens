42eb6f6646d3a08c089ba61d6769bf4adfeb09d1
Cametized <cameron.reese.f@gmail.com>
2026-05-08 22:57:06 -0400

    Uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- src/main/java/claire/gens/ItemStuff.java
+++ src/main/java/claire/gens/ItemStuff.java
@@ -20,6 +20,17 @@ public class ItemStuff {
     }
     public static final Item RadicalRadio = register("radical", Item::new, new Item.Properties());
 
+    public static final Item strength = register("strengthfrag", Item::new, new Item.Properties());
+    public static final Item swiftness = register("swiftnessfrag", Item::new, new Item.Properties());
+    public static final Item haste = register("hastefrag", Item::new, new Item.Properties());
+    public static final Item jumpboos = register("jumpfrag", Item::new, new Item.Properties());
+    public static final Item resist = register("resistancefrag", Item::new, new Item.Properties());
+    public static final Item absorption = register("extraheartfrag", Item::new, new Item.Properties());
+    public static final Item fireres = register("fireresfrag", Item::new, new Item.Properties());
+    public static final Item nightvision = register("nightvisionfrag", Item::new, new Item.Properties());
+    public static final Item waterbreath = register("waterbreathefrag", Item::new, new Item.Properties());
+    public static final Item invis = register("invisfrag", Item::new, new Item.Properties());
+
     public static void initialize() {}
 }
 
