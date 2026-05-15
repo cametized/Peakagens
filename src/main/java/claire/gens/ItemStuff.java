49e4dd502014671dbb75104b81674f9fb1300c53
Cametized <cameron.reese.f@gmail.com>
2026-05-14 22:28:53 -0400

    i tried lets see

--- src/main/java/claire/gens/ItemStuff.java
+++ src/main/java/claire/gens/ItemStuff.java
@@ -2,9 +2,9 @@ package claire.gens;
 
 import net.fabricmc.fabric.api.creativetab.v1.FabricCreativeModeTab;
 import net.minecraft.core.Registry;
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
@@ -13,12 +13,17 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.nbt.DoubleTag;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ToolMaterial;
+import net.minecraft.world.item.equipment.ArmorType;
 import net.minecraft.world.item.alchemy.PotionContents;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.function.Function;
+import claire.gens.CloakMaterial;
 
 public class ItemStuff {
     public static <T extends Item> T register(String name, Function<Item.Properties, T> itemFactory, Item.Properties settings) {
@@ -29,10 +34,41 @@ public class ItemStuff {
         return item;
     }
 
+    public static final ToolMaterial angels_material = new ToolMaterial(
+            BlockTags.INCORRECT_FOR_WOODEN_TOOL,
+            1024,
+            5.0F,
+            2F,
+            22,
+            ToolMaterial.GOLD.repairItems()
+    );
+
+    public static final ToolMaterial cloakermat = new ToolMaterial(
+            BlockTags.INCORRECT_FOR_WOODEN_TOOL,
+            455,
+            5.0F,
+            1.5F,
+            22,
+            CloakMaterial.REPAIRS_CLOAKER
+    );
+
     public static final Item RadicalRadio = register(
             "radical",
             Item::new,
             new Item.Properties()
+    );
+
+    public static final Item angelsword = register(
+            "angels_longsword",
+            Item::new,
+            new Item.Properties().sword(angels_material,5f,-2f)
+    );
+
+    public static final Item cloaker_mask = register(
+            "cloaker_hood",
+            Item::new,
+            new Item.Properties().humanoidArmor(CloakMaterial.INSTANCE, ArmorType.HELMET)
+                    .durability(ArmorType.HELMET.getDurability(CloakMaterial.BASE_DURABILITY))
     );
 
     public static final Item ricebowl = register(
@@ -163,11 +199,10 @@ public class ItemStuff {
             .icon(() -> new ItemStack(ItemStuff.friedegg))
             .title(Component.translatable("creativeTab.peakagens"))
             .displayItems((params, output) -> {
-                output.accept(RadicalRadio);
-
-                output.accept(ricebowl);
-                output.accept(friedegg);
-                output.accept(bacon);
+                output.accept(ItemStuff.RadicalRadio);
+                output.accept(ItemStuff.ricebowl);
+                output.accept(ItemStuff.friedegg);
+                output.accept(ItemStuff.bacon);
 
                 List<Item> hey = List.of(strength,haste,swiftness,jumpboos,fireres,resist,absorption,nightvision,waterbreath,invis);
                 for (int i = 0; i < hey.size()-1; i++) {
@@ -192,7 +227,9 @@ public class ItemStuff {
                 }
 
                 output.accept(alchemy);
-                output.accept(lifesteal);
+
+                output.accept(ItemStuff.angelsword);
+                output.accept(ItemStuff.cloaker_mask);
             })
             .build();
 
