2e8ab27753d059982108310a407f4698e5eff8b5
Cametized <cameron.reese.f@gmail.com>
2026-05-23 19:30:27 -0400

    fat booty bitches

--- src/main/java/claire/gens/LootTables.java
+++ src/main/java/claire/gens/LootTables.java
@@ -1,6 +1,7 @@
 package claire.gens;
 
 import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.storage.loot.BuiltInLootTables;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
@@ -20,7 +21,31 @@ public class LootTables {
                 LootPool.Builder poolBuilder = LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(1))
                         .when(LootItemRandomChanceCondition.randomChance(0.25f))
+                        .add(LootItem.lootTableItem(ItemStuff.yag));
+                tableBuilder.withPool(poolBuilder);
+            } else if (BuiltInLootTables.IGLOO_CHEST.equals(key) &&  source.isBuiltin()) {
+                LootPool.Builder poolBuilder = LootPool.lootPool()
+                        .setRolls(ConstantValue.exactly(1))
+                        .when(LootItemRandomChanceCondition.randomChance(0.75f))
+                        .add(LootItem.lootTableItem(ItemStuff.cleanup));
+                tableBuilder.withPool(poolBuilder);
+            } else if (BuiltInLootTables.NETHER_BRIDGE.equals(key) &&  source.isBuiltin()) {
+                LootPool.Builder poolBuilder = LootPool.lootPool()
+                        .setRolls(ConstantValue.exactly(1))
+                        .when(LootItemRandomChanceCondition.randomChance(0.50f))
+                        .add(LootItem.lootTableItem(ItemStuff.disc1));
+                tableBuilder.withPool(poolBuilder);
+            } else if (BuiltInLootTables.PILLAGER_OUTPOST.equals(key) &&  source.isBuiltin()) {
+                LootPool.Builder poolBuilder = LootPool.lootPool()
+                        .setRolls(ConstantValue.exactly(1))
+                        .when(LootItemRandomChanceCondition.randomChance(0.65f))
                         .add(LootItem.lootTableItem(ItemStuff.test11));
+                tableBuilder.withPool(poolBuilder);
+            } else if (BuiltInLootTables.SHIPWRECK_TREASURE.equals(key) &&  source.isBuiltin()) {
+                LootPool.Builder poolBuilder = LootPool.lootPool()
+                        .setRolls(ConstantValue.exactly(1))
+                        .when(LootItemRandomChanceCondition.randomChance(0.35f))
+                        .add(LootItem.lootTableItem(Items.ENCHANTED_BOOK));
                 tableBuilder.withPool(poolBuilder);
             }
         });
