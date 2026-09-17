c0e23dc28041ef651084a973bb2e9cb25d4a1ddd
Cametized <cameron.reese.f@gmail.com>
2026-09-17 19:30:11 -0400

    loot

--- src/main/java/claire/gens/LootTables.java
+++ src/main/java/claire/gens/LootTables.java
@@ -14,7 +14,8 @@ import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
 import net.minecraft.world.level.storage.loot.functions.LootItemFunction;
 import net.minecraft.world.level.storage.loot.functions.SetEnchantmentsFunction;
-import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
+import net.minecraft.world.level.storage.loot.providers.number.floats.ConstantValue;
+import net.minecraft.world.level.storage.loot.providers.number.ints.ContextIntProviders;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 
 import java.util.List;
@@ -26,49 +27,49 @@ public class LootTables {
         LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
             if (BuiltInLootTables.ABANDONED_MINESHAFT.equals(key) && source.isBuiltin()) {
                 LootPool.Builder poolBuilder = LootPool.lootPool()
-                        .setRolls(ConstantValue.exactly(1))
+                        .setRolls(ContextIntProviders.exactly(1))
                         .when(LootItemRandomChanceCondition.randomChance(0.15f))
                         .add(LootItem.lootTableItem(ItemStuff.grindrails));
                 tableBuilder.withPool(poolBuilder);
             } else if (List.of(BuiltInLootTables.TRIAL_CHAMBERS_CORRIDOR_POT,BuiltInLootTables.TRIAL_CHAMBERS_INTERSECTION_BARREL,BuiltInLootTables.TRIAL_CHAMBERS_CHAMBER_DISPENSER).equals(key) &&  source.isBuiltin()) {
                 LootPool.Builder poolBuilder = LootPool.lootPool()
-                        .setRolls(ConstantValue.exactly(1))
+                        .setRolls(ContextIntProviders.exactly(1))
                         .when(LootItemRandomChanceCondition.randomChance(0.75f))
                         .add(LootItem.lootTableItem(ItemStuff.colonize));
                 tableBuilder.withPool(poolBuilder);
             } else if (BuiltInLootTables.IGLOO_CHEST.equals(key) &&  source.isBuiltin()) {
                 LootPool.Builder poolBuilder = LootPool.lootPool()
-                        .setRolls(ConstantValue.exactly(1))
+                        .setRolls(ContextIntProviders.exactly(1))
                         .when(LootItemRandomChanceCondition.randomChance(0.75f))
                         .add(LootItem.lootTableItem(ItemStuff.cleanup));
                 tableBuilder.withPool(poolBuilder);
             } else if (BuiltInLootTables.NETHER_BRIDGE.equals(key) &&  source.isBuiltin()) {
                 LootPool.Builder poolBuilder = LootPool.lootPool()
-                        .setRolls(ConstantValue.exactly(1))
+                        .setRolls(ContextIntProviders.exactly(1))
                         .when(LootItemRandomChanceCondition.randomChance(0.50f))
                         .add(LootItem.lootTableItem(ItemStuff.disc1));
                 tableBuilder.withPool(poolBuilder);
             } else if (BuiltInLootTables.PILLAGER_OUTPOST.equals(key) &&  source.isBuiltin()) {
                 LootPool.Builder poolBuilder = LootPool.lootPool()
-                        .setRolls(ConstantValue.exactly(1))
+                        .setRolls(ContextIntProviders.exactly(1))
                         .when(LootItemRandomChanceCondition.randomChance(0.65f))
                         .add(LootItem.lootTableItem(ItemStuff.test11));
                 tableBuilder.withPool(poolBuilder);
             } else if (BuiltInLootTables.SHIPWRECK_TREASURE.equals(key) &&  source.isBuiltin()) {
                 LootPool.Builder poolBuilder = LootPool.lootPool()
-                        .setRolls(ConstantValue.exactly(1))
+                        .setRolls(ContextIntProviders.exactly(1))
                         .when(LootItemRandomChanceCondition.randomChance(0.5f))
-                        .add(LootItem.lootTableItem(Items.ENCHANTED_BOOK).apply(new SetEnchantmentsFunction.Builder().withEnchantment(registries.lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(ResourceKey.create(Registries.ENCHANTMENT, Identifier.fromNamespaceAndPath(FloristEnchant.MOD_ID, "florist"))), ConstantValue.exactly(1))));
+                        .add(LootItem.lootTableItem(Items.ENCHANTED_BOOK).apply(new SetEnchantmentsFunction.Builder().withEnchantment(registries.lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(ResourceKey.create(Registries.ENCHANTMENT, Identifier.fromNamespaceAndPath(FloristEnchant.MOD_ID, "florist"))), ContextIntProviders.exactly(1))));
                 tableBuilder.withPool(poolBuilder);
             } else if (BuiltInLootTables.SIMPLE_DUNGEON.equals(key) &&  source.isBuiltin()) {
                 LootPool.Builder poolBuilder = LootPool.lootPool()
-                        .setRolls(ConstantValue.exactly(1))
+                        .setRolls(ContextIntProviders.exactly(1))
                         .when(LootItemRandomChanceCondition.randomChance(0.10f))
                         .add(LootItem.lootTableItem(ItemStuff.cloth));
                 LootPool.Builder poolBuilder1 = LootPool.lootPool()
-                        .setRolls(ConstantValue.exactly(1))
+                        .setRolls(ContextIntProviders.exactly(1))
                         .when(LootItemRandomChanceCondition.randomChance(0.35f))
-                        .add(LootItem.lootTableItem(Items.ENCHANTED_BOOK).apply(new SetEnchantmentsFunction.Builder().withEnchantment(registries.lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(ResourceKey.create(Registries.ENCHANTMENT, Identifier.fromNamespaceAndPath(FloristEnchant.MOD_ID, "florist"))), ConstantValue.exactly(1))));
+                        .add(LootItem.lootTableItem(Items.ENCHANTED_BOOK).apply(new SetEnchantmentsFunction.Builder().withEnchantment(registries.lookupOrThrow(Registries.ENCHANTMENT).getOrThrow(ResourceKey.create(Registries.ENCHANTMENT, Identifier.fromNamespaceAndPath(FloristEnchant.MOD_ID, "florist"))), ContextIntProviders.exactly(1))));
                 tableBuilder.withPool(poolBuilder).withPool(poolBuilder1);
             } else if (BuiltInLootTables.TRIAL_CHAMBERS_REWARD_OMINOUS_UNIQUE.equals(key) && source.isBuiltin()) {
                 LootItemFunction lootItemFunction = new LootItemFunction() {
@@ -86,7 +87,7 @@ public class LootTables {
                 tableBuilder.apply(lootItemFunction);
             } else if (List.of(BuiltInLootTables.BASTION_TREASURE,BuiltInLootTables.BASTION_BRIDGE,BuiltInLootTables.BASTION_OTHER,BuiltInLootTables.BASTION_HOGLIN_STABLE).contains(key) && source.isBuiltin()) {
                 LootPool.Builder poolBuilder = BuiltInLootTables.BASTION_TREASURE.equals(key) ?
-                        LootPool.lootPool().setRolls(ConstantValue.exactly(1)).when(LootItemRandomChanceCondition.randomChance(0.25f)).add(LootItem.lootTableItem(ItemStuff.yag))
+                        LootPool.lootPool().setRolls((ContextIntProviders.exactly(1))).when(LootItemRandomChanceCondition.randomChance(0.25f)).add(LootItem.lootTableItem(ItemStuff.yag))
                         : LootPool.lootPool();
                 LootItemFunction lootItemFunction = new LootItemFunction() {
                     @Override
@@ -103,7 +104,7 @@ public class LootTables {
                 tableBuilder.apply(lootItemFunction).withPool(poolBuilder);
             } else if (key.equals(BuiltInLootTables.CHARGED_CREEPER_WITHER_SKELETON) && source.isBuiltin()) { // i told you i'd make it hard asf to get
                 LootPool.Builder poolBuilder = LootPool.lootPool()
-                        .setRolls(ConstantValue.exactly(1))
+                        .setRolls(ContextIntProviders.exactly(1))
                         .when(LootItemRandomChanceCondition.randomChance(0.25f))
                         .add(LootItem.lootTableItem(ItemStuff.flaxsong));
                 tableBuilder.withPool(poolBuilder);
